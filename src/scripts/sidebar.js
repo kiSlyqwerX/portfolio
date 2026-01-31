0ee21751f7d1928ed10ca08d230fb3ac219bf739
Yura <yuraostab@gmail.com>
2026-01-31 12:00:10 +0100

    optimisation, sidebar changes

--- src/scripts/sidebar.js
+++ src/scripts/sidebar.js
@@ -1,6 +1,13 @@
 function initSidebar() {
     const sidebar = document.querySelector("aside.sidebar")
     const sidebarToggler = document.querySelector("#sidebar-toggler")
+    const allLinks = sidebar.querySelectorAll("a")
+
+    allLinks.forEach(a => {
+        a.addEventListener("click", () => {
+            sidebar.classList.remove("show")
+        })
+    })
 
     sidebarToggler.addEventListener("click", () => {
         sidebar.classList.toggle("show")
